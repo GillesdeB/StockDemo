87afff59a3a25676221d54715e3797c6d4ac506f
Gilles de Bordeaux <gillesdebordeaux@hotmail.com>
2017-01-21 15:52:10 -0800

    Initial commit.

---
+++ StockDemo/StockDemo/Model/QuoteData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockDemo.Model
+{
+    public class QuoteData
+    {
+        public string CurrentQuote { get; set; }
+        public string Change { get; set; }
+        public string Company { get; set; }
+        public string YearRange { get; set; }
+    }
+
+}
---
+++ StockDemo/StockDemo/ViewModel/StockViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using StockDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Net.Http;
+
+namespace StockDemo.ViewModel
+{
+    public class StockViewModel
+    {
+        public QuoteData data { get; set; } = null;
+        public bool IsBusy { get; set; } = false;
+        public async Task<bool> GetQuote(string ticker)
+        {
+            if (IsBusy) { return false};
+
+            try
+            {
+                IsBusy = true;
+                var url = "https://motzstocks2.azurewebsites.net/api/"
+                    + $"HttpTriggerSharp1?ticker={ticker}";
+
+                var client = new HttpClient();
+                var json = await client.GetStringAsync(url);
+
+                Data = JsonConvert.DeserializeObject<QuoteData>(json);
+            }
+            catch (Exception ex)
+            {
+                data = null;
+                return false;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+            return true;
+        }
+    }
+}
