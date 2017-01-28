98865c7255bf400e34f62fc72da4f4e11c86392d
Gilles de Bordeaux <gillesdebordeaux@hotmail.com>
2017-01-27 17:04:59 -0800

    Adding more stock quote providers

--- StockDemo/StockDemo/ViewModel/StockViewModel.cs
+++ StockDemo/StockDemo/ViewModel/StockViewModel.cs
@@ -23,6 +23,19 @@ namespace StockDemo.ViewModel
                 var url = "https://motzstocks2.azurewebsites.net/api/"
                     + $"HttpTriggerSharp1?ticker={ticker}";
 
+                // https://www.codeproject.com/articles/37550/stock-quote-and-chart-from-yahoo-in-c
+                //string yahooUrl = @"http://download.finance.yahoo.com/d/quotes.csv?s=" +
+                //                  ticker + "&f=sl1d1t1c1hgvbap2";
+                //string[] symbols = symbol.Replace(",", " ").Split(' ');
+                //url = yahooUrl;
+                // https://www.codeproject.com/articles/618516/stock-quotes-and-charts-from-google-finance-using
+                //ticker = ticker.Trim();
+                //ticker = ticker.Replace(" ", "&stock=");
+                //ticker = ticker.Replace(",", "&stock=");
+                //var url = "https://www.google.com/ig/api?stock=" + (ticker);
+                // https://www.quantshare.com/sa-426-6-ways-to-download-free-intraday-and-tick-data-for-the-us-stock-market
+                // https://www.quantshare.com/sa-43-10-ways-to-download-historical-stock-quotes-data-for-free
+
                 var client = new HttpClient();
                 var json = await client.GetStringAsync(url);
 
